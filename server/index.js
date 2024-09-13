bf8caa6dfe2f07a2a70b93f398f4bbd60bca27bb
karan singh D <ceitkaransingh2026@gmail.com>
2024-09-13 19:59:26 +0530

    origin

--- server/index.js
+++ server/index.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(cors({
-  origin: 'https://aconews-585dd.web.app/'  // Replace with your actual frontend domain if deployed
-}));
+
 
 // Use routes
 app.use('/news', newsRoutes);
--- server/routes/suggestion.js
+++ server/routes/suggestion.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {getSuggestions} = require('../controllers/suggestionscontroller');
-s
+
 router.get('/', getSuggestions);
 
 module.exports = router;
